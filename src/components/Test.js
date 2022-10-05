57d932b32fca9f0a8056c20485a2d10836b9702e
pawpawmasterpiece <ccoldplayy07@gmail.com>
2022-10-05 15:21:23 +0700

    initial commit

---
+++ src/components/Test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div className='header'>
+        <div className='container'>
+            <div className='header-con'>
+                <div className='logo-container'>
+                    <a href='#'>PAWPAW</a>
+                </div>
+                <ul className='menu'>
+                    <li className='menu-link'>
+                        <a href='#'>A</a>
+                    </li>
+                    <li className='menu-link'>
+                        <a href='#'>B</a>
+                    </li>
+                    <li className='menu-link'>
+                        <a href='#'>C</a>
+                    </li>
+                    <li className='menu-link'>
+                        <a href='#'>D</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Test
